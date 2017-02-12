6661f19f0f8102d7498a26bb2666acc715b5c197
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-11 22:15:29 -0700

    Log in passing spec

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "You are successfully logged in as #{user.first_name}."
+      redirect_to user_path(user)
+    else
+      render :new
+    end
+  end
+
+end
--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -3,12 +3,13 @@ require "rails_helper"
 describe "user logs in" do
   context "they visit the login path" do
     it "logs in a user" do
-      User.create(first_name: "A", last_name: "S", email: "email@email.com", password: "password", password_confirmation: "password")
+      user = User.create(first_name: "A", last_name: "S", email: "email@email.com", password: "password", password_confirmation: "password")
 
       visit login_path
 
-      fill_in "user[email]", with: "email@email.com"
-      fill_in "user[password]", with: "password"
+      fill_in "email", with: "email@email.com"
+      fill_in "password", with: "password"
+      click_on "Log In"
 
       expect(current_path).to eq(user_path(user))
       expect(page).to have_content("Welcome A S!")
