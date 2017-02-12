cf84cb827c8b5020f78322371257d61d2f25db0e
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-11 21:54:10 -0700

    Create user spec passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -7,10 +11,10 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      binding.pry
+      redirect_to user_path(user)
     else
       render :new
-    end 
+    end
   end
 
   private
